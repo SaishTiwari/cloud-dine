7ef7ef300d33a14491bf0e9c15851e574352e4ec
SaishTiwari <tiwarisaish381@gmail.com>
2026-05-20 21:18:31 -0400

    auth service intial setup

---
+++ auth-service/src/test/java/com/clouddine/auth/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clouddine.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
